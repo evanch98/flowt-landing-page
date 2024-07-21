347b506bf33961cf559a4595833422c99a2ccd95
Kyaw Thu <evanch98@gmail.com>
2024-07-21 18:13:11 +0800

    changing the background color

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -4,6 +4,19 @@ import type { Preview } from '@storybook/react';
 
 const preview: Preview = {
   parameters: {
+    backgrounds: {
+      default: 'black',
+      values: [
+        {
+          name: 'black',
+          value: '#0F172A',
+        },
+        {
+          name: 'white',
+          value: '#FFFFFF',
+        },
+      ],
+    },
     controls: {
       matchers: {
         color: /(background|color)$/i,
