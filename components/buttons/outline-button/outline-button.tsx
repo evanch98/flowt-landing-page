54632a7ba8ca2acd8ceb15477c2cb71ce4b66193
Kyaw Thu <evanch98@gmail.com>
2024-07-21 17:13:23 +0800

    using HTMLButtonElement props

--- components/buttons/outline-button/outline-button.tsx
+++ components/buttons/outline-button/outline-button.tsx
@@ -1,17 +1,22 @@
 import { cn } from '@/lib/utils';
 
-interface OutlineButtonProps {
+interface OutlineButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: string;
   className?: string;
 }
 
-export const OutlineButton = ({ children, className }: OutlineButtonProps) => {
+export const OutlineButton = ({
+  children,
+  className,
+  ...props
+}: OutlineButtonProps) => {
   return (
     <button
       className={cn(
         'group rounded-full border-2 border-white p-3 transition-colors delay-100 duration-500 ease-[cubic-bezier(0.19,1,0.22,1)] hover:bg-white hover:bg-opacity-100',
         className,
       )}
+      {...props}
     >
       <div className="relative mx-4 overflow-hidden text-buttonM">
         <p className="text-white duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)] group-hover:translate-y-[-20px]">
--- components/buttons/primary-button/primary-button.tsx
+++ components/buttons/primary-button/primary-button.tsx
@@ -1,17 +1,22 @@
 import { cn } from '@/lib/utils';
 
-interface PrimaryButtonProps {
+interface PrimaryButtonProps extends React.HTMLAttributes<HTMLButtonElement> {
   children: string;
   className?: string;
 }
 
-export const PrimaryButton = ({ children, className }: PrimaryButtonProps) => {
+export const PrimaryButton = ({
+  children,
+  className,
+  ...props
+}: PrimaryButtonProps) => {
   return (
     <button
       className={cn(
         'group rounded-full bg-white p-3 transition-colors delay-100 duration-500 ease-[cubic-bezier(0.19,1,0.22,1)] hover:bg-neutral-500',
         className,
       )}
+      {...props}
     >
       <div className="relative mx-4 overflow-hidden text-buttonM">
         <p className="text-black duration-[1.125s] ease-[cubic-bezier(0.19,1,0.22,1)] group-hover:translate-y-[-20px]">
