f0ce47f8a1b96bcb94393addee933ae44d3b5d0e
Kyaw Thu <evanch98@gmail.com>
2024-07-26 15:37:20 +0800

    updating the hero section

--- components/sections/hero/hero-section.tsx
+++ components/sections/hero/hero-section.tsx
@@ -1,10 +1,12 @@
+import { FeatureSection } from './feature-section/feature-section';
 import { IntroSection } from './intro-section/intro-section';
 
 export const HeroSection = () => {
   return (
     <section className="mt-52 flex h-full w-full flex-col items-center justify-center">
-      <div className="flex w-full max-w-7xl flex-col px-4 xl:px-8">
+      <div className="flex w-full max-w-7xl flex-col gap-y-6 px-4 md:gap-y-7 xl:px-8">
         <IntroSection />
+        <FeatureSection />
       </div>
     </section>
   );
