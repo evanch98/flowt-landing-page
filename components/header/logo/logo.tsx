5e46ff7a6afd424a9be68ee1f3c445229ec06a17
Kyaw Thu <evanch98@gmail.com>
2024-07-26 12:21:30 +0800

    updating the style

--- components/header/logo/logo.tsx
+++ components/header/logo/logo.tsx
@@ -6,7 +6,7 @@ interface LogoProps extends React.HTMLAttributes<HTMLDivElement> {}
 export const Logo = ({ className, ...props }: LogoProps) => {
   return (
     <div
-      className={cn('text-3xl md:text-4xl font-bold', className)}
+      className={cn('text-3xl font-bold md:text-4xl', className)}
       {...props}
     >
       <Link href="/">flowt</Link>
