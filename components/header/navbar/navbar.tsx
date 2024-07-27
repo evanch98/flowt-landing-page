73b0e0698a67245c8f9bb517423d81aa0c0d0c02
Kyaw Thu <evanch98@gmail.com>
2024-07-27 15:50:09 +0800

    updating the style

--- components/header/navbar/navbar.tsx
+++ components/header/navbar/navbar.tsx
@@ -43,26 +43,28 @@ export const Navbar = () => {
         animate={hidden ? 'hidden' : 'visible'}
         transition={{ duration: 0.35, ease: 'easeInOut' }}
         className={cn(
-          'fixed top-0 z-50 flex w-full items-center justify-between p-4 md:px-8 md:py-6 xl:px-20',
+          'fixed top-0 z-50 flex w-full items-center justify-center',
           scrolled && 'bg-neutral-900 shadow-md',
         )}
       >
-        {/* Large Screen */}
-        <div className="hidden items-center gap-x-12 lg:flex">
-          <Logo />
-          <SlidingTabs />
+        <div className="flex w-full max-w-7xl items-center justify-between px-4 py-2 md:py-4 xl:px-8">
+          {/* Large Screen */}
+          <div className="hidden items-center gap-x-12 lg:flex">
+            <Logo />
+            <SlidingTabs />
+          </div>
+          <div className="hidden items-center gap-x-4 lg:flex">
+            <OutlineButton>Log In</OutlineButton>
+            <PrimaryButton>Publish</PrimaryButton>
+          </div>
+          {/* Small Screen */}
+          <div className="flex-shrink-0 lg:hidden">
+            <Logo />
+          </div>
+          <MobileNavbar>
+            <HiMenu className="size-8" />
+          </MobileNavbar>
         </div>
-        <div className="hidden items-center gap-x-4 lg:flex">
-          <OutlineButton>Log In</OutlineButton>
-          <PrimaryButton>Publish</PrimaryButton>
-        </div>
-        {/* Small Screen */}
-        <div className="flex-shrink-0 lg:hidden">
-          <Logo />
-        </div>
-        <MobileNavbar>
-          <HiMenu className="size-8" />
-        </MobileNavbar>
       </motion.nav>
     </ClientProvider>
   );
